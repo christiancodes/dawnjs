b5a407a22da2b19eb66bdff0bd64d283a5311a7e
Christian DeWolf <christian@wolfbutler.com>
2016-08-10 20:22:50 -0300

    Image name

--- pages/i-00-01.php
+++ pages/i-00-01.php
@@ -1,7 +1,7 @@
 <div class="chapter">
 
   <div class="center">
-    <img src="images/01.jpg" class="story-image" width="500" height="350">
+    <img src="images/i00.01.png" class="story-image" width="500" height="350">
   </div>
 
   <p class="bigger-text">To you, my comrade.</p>
