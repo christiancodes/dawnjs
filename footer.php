296f7a7465e26e4d03fcada3e8b0e96959f8ece9
Christian DeWolf <christian@wolfbutler.com>
2016-08-10 17:59:31 -0300

    Social buttons

--- footer.php
+++ footer.php
@@ -1,9 +1,30 @@
+<script>window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));</script>
+
 <div id="footer-icons">
   <a href="http://www.patreon.com/playerprophet">
     <img src="images/patreon.png" alt="Patreon" width="33" height="33">
   </a>
 
-  <!-- TODO insert social icons -->
+  <iframe src="https://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fplayerprophet.com%2Fthosewithoutshadows%2F&width=61&layout=button_count&action=like&size=small&show_faces=false&share=false&height=21" width="61" height="21" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+
+  <a class="twitter-share-button"
+    href="https://twitter.com/intent/tweet?text=I'm%20reading%20Those%20Without%20Shadows%3A">
+  Tweet</a>
 
   <a href="twsfeed.xml">
     <img src="images/rss.png" alt="RSS" width="33" height="33">
