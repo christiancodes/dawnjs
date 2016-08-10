bbeaa3cd49f381de3606ebce4a0c6dcb6543d9b2
Christian DeWolf <christian@wolfbutler.com>
2016-08-10 20:06:57 -0300

    Try a responsive fix

--- pages/i-00-01.php
+++ pages/i-00-01.php
@@ -1,6 +1,6 @@
 <div class="chapter">
 
-  <p>To you, my comrade.</p>
+  <p class="bigger-text">To you, my comrade.</p>
 
   <div class="center">
     <img src="images/01.jpg" class="story-image" width="500" height="350">
