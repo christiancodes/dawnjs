869411fc03d8040426bd5211d6be7e5457fcd3cb
Christian DeWolf <christian@wolfbutler.com>
2016-09-05 10:14:42 -0300

    Add analytics

--- tws_scripts.php
+++ tws_scripts.php
@@ -1,3 +1,4 @@
+<script type="application/javascript" src="scripts/analytics.js"></script>
 <script type="application/javascript" src="scripts/contents.js"></script>
 <script type="application/javascript" src="scripts/shadows.js"></script>
 <script type="application/javascript" src="scripts/progress.js"></script>
